1156549cfc98a4053143c888f5d7039c9f9c4dc2
Krishnamurthy Iyer <iyerkri@gmail.com>
2023-01-13 17:39:00 -0600

    sway launcher

---
+++ startsway.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Set environment variables for wayland
+
+export XDG_CURRENT_DESKTOP=sway
+export XDG_SESSION_DESKTOP="sway"
+export XDG_SESSION_TYPE="wayland"
+export GDK_BACKEND="wayland,x11"
+export MOZ_ENABLE_WAYLAND=1
+
+export TERMINAL="foot"
+export EDITOR="emacsclient -t -a emacs"
+export VISUAL="emacsclient -c -a emacs"
+export BROWSER="firefox"
+
+exec sway
